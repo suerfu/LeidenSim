086a082660eaae03672d16be95fc5a8bb71142cd
xinran <xinranli@lbl.gov>
2021-11-11 22:22:56 -0800

    adding geometry WIP

--- src/GeoCryostat.cc
+++ src/GeoCryostat.cc
@@ -54,12 +54,87 @@ void GeoCryostat::ConstructTanks(){
 
 void GeoCryostat::ConstructColdParts(){
 	G4String name = "Cryostat";
-	//Still plate
-	G4Tubs* stillPlateSolid = new G4Tubs( name+"StillPlateTubs", 
-										0, 
-										Geomanager::Get()->GetDimensions("stillPlateRadius"),
-										0, 2*MY_PI);
-	G4LogicalVolume * 
+
+	//Holes
+	G4int nHoleSizes = GeoManager::Get()->GetNumberOfHoleSizes();
+	G4Tubs** holes = new G4Tubs*[nHoleSizes];
+	for(int ih=0; ih<nHoleSizes; ih++){
+		holes[ih] = new G4Tubs( name+"Hole",
+								0, 
+								GeoManager::Get()->GetHoleRadius(ih),
+								GeoManager::Get()->GetDimensions("maxPlateThickness")/2,//make sure to drill through all plates.
+								0, 2*MY_PI);
+	}
+	//Plate with holes
+	for(int ip=0; ip<GeoManager::Get()->GetNumberOfCryoPlates(); ip++){
+		G4Tubs* plate = new G4Tubs( name+GeoManager::Get()->GetCryoPlateName(ip), 
+									0, 
+									GeoManager::Get()->GetDimensions(GeoManager::Get()->GetCryoPlateName(ip)+"Radius"),
+									GeoManager::Get()->GetDimensions(GeoManager::Get()->GetCryoPlateName(ip)+"Thickness")/2,
+									0, 2*MY_PI);
+		G4VSolid* plateWithHoles = plate;
+		G4int nHoles = GeoManager::Get()->GetNumberOfCryoPlateHoles(ip);
+		//drill holes
+		for(int ih=0; ih<nHoles; i++){
+			plateWithHoles = new G4SubtractionSolid( name+GeoManager::Get()->GetCryoPlateName(ip),
+													plateWithHoles, 
+													holes[GeoManager::Get()->GetCryoPlateHoleSize(ip,ih)],
+													0,
+													GeoManager::Get()->GetCryoPlateHolePos(ip, ih),
+													);
+		}
+		G4LogicalVolume * plateLogic = new G4LogicalVolume( plateWithHoles,
+													GeoManager::Get()->GetCryoPlateMaterial(ip),
+													name+GeoManager::Get()->GetCryoPlateName(ip)+"LV");
+		G4VPhysicalVolume* platePhysical = new G4PVPlacement(0,
+													G4ThreeVector(0,0,GeoManager::Get()->GetCryoPlateZ(ip)),
+													plateLogic,
+													name+GeoManager::Get()->GetCryoPlateName(ip)+"Physical",
+													motherLogic,
+													false,
+													0,
+													fCheckOverlaps);
+		GeoManager::Get()->Add( name+GeoManager::Get()->GetCryoPlateName(ip),
+								plateLogic, platePhysical);
+	}
+	//Support beams
+	for(int ib=0; ib<GeoManager::Get()->GetNumberOfCryoBeams(); ip++){
+		G4Tubs* beam = new G4Tubs( name+GeoManager::Get()->GetCryoBeamName(ip),
+									0,
+									GeoManager::Get()->GetDimensions(GeoManager::Get()->GetCryoBeamName(ib)+"Radius"),
+									GeoManager::Get()->GetDimensions(GeoManager::Get()->GetCryoBeamName(ib)+"Height")/2,
+									0, 2*MY_PI);
+		G4LogicalVolume * beamLogic = new G4LogicalVolume( beam,
+									GeoManager::Get()->GetCryoBeamMaterial(ib),
+									name+GeoManager::Get()->GetCryoBeamName(ib)+"LV");
+		G4VPhysicalVolume* beamPhysical = new G4PVPlacement(0,
+									GeoManager::Get()->GetCryoBeamPos(ib),
+									beamLogic,
+									name+GeoManager::Get()->GetCryoBeamName(ib)+"Physical",
+									motherLogic,
+									false,
+									0,
+									fCheckOverlaps);
+		GeoManager::Get()->Add( name+GeoManager::Get()->GetCryoBeamName(ib),
+								beamLogic, beamPhysical);
+	}
+	//Mixing chammber (inner most shield). Top and bottom are placed as "plates with holes"!
+	G4Tubs* MXCWallTubs = new G4Tubs( name+"MXCWallTubs", 
+									GeoManager::Get()->GetDimensions("MXCWallInnerRadius"),
+									GeoManager::Get()->GetDimensions("MXCWallOuterRadius"),
+									GeoManager::Get()->GetDimensions("MXCWallHeight")/2,
+									0, 2*MY_PI);
+	G4LogicalVolume * MXCWallLogic = new G4LogicalVolume( MXCWallTubs, 
+									GeoManager::Get()->GetMaterial("Cu"),
+									name+"MXCWallLV");
+	G4VPhysicalVolume* MXCWallPhysical = new G4PVPlacement(0,
+									G4ThreeVector(0, 0, GeoManager::Get()->GetDimensions("detectorZOffset")),
+									MXCWallLogic,
+									name + "MXCWallPhysical",
+									motherLogic,
+									false,
+									0,
+									fCheckOverlaps);
 	
 }
 
