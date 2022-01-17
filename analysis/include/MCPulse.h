d319fc39f4413329f8f37d79b7aaa0a78eda5bdc
Burkhant Suerfu <suerfu@berkeley.edu>
2022-01-16 16:05:01 -0800

    Split into pulse and pulse array source file.

--- analysis/include/MCPulse.h
+++ analysis/include/MCPulse.h
@@ -11,91 +11,36 @@ class MCPulse{
 
 public:
 
-	static unsigned int GetNType(){ return nType;}
+	static unsigned int GetNType(){ return nType; }
 
-	MCPulse(){
-		time = -1;
-		for(unsigned int i=0; i<nType; i++){
-			energy.push_back(0);
-		}
-	}
+	MCPulse();
 
-	MCPulse( double t, vector<double> a){
-		time = t;
+	MCPulse( double t, vector<double> a);
 
-		unsigned int m = nType<a.size()?nType:a.size();
-		for(unsigned int i=0; i<m; i++)
-			energy.push_back( a[i] );
-		for(unsigned int i=m+1; i<nType; i++)
-			energy.push_back( 0 );
-		
-	}
+	MCPulse( const MCPulse& a);
 
-	MCPulse( const MCPulse& a){
-		time = a.time;
-		energy = a.energy;
-	}
+	~MCPulse();
 
-	~MCPulse(){}
+	MCPulse& operator=( const MCPulse& a);
 
-	MCPulse& operator=( const MCPulse& a){
-		time = a.time;
-		energy = a.energy;
-		return *this;
-	}
+	bool operator<( const MCPulse& a ){ return time < a.time; }
 
-	bool operator<( const MCPulse& a ){
-		return time < a.time;
-	}
+	MCPulse& operator+( const MCPulse& a);
 
-	MCPulse& operator+( const MCPulse& a){
+	void operator+=( const MCPulse& a);
 
-		if( time<0 ){
-			time = a.time;
-		}
-		else{
-			time = time<a.time?time:a.time;
-		}
-
-		for(unsigned int i=0; i<nType; i++){
-			energy[i] += a.energy[i];
-		}
-
-		return (*this);
-	}
-
-	void operator+=( const MCPulse& a){
-		if( time<0 )
-			time = a.time;
-		else
-			time = time<a.time ? time : a.time;
-
-		for(unsigned int i=0; i<nType; i++){
-			energy[i] += a.energy[i];
-		}
-	}
-
-
-	void SetTime( double a ){
-		time = a;
-	}
+	void SetTime( double a ){ time = a; }
 	
-	double GetTime(){
-		return time;
-	}
+	double GetTime(){ return time; }
 
-	void SetEnergy( int i, double a ){
-		if(i<nType) energy[i] = a;
-	}
+	void SetEnergy( int i, double a ){ if(i<nType) energy[i] = a; }
 	
 	void SetEnergy( vector<double> a ){
 		if(a.size()==nType)
 			energy = a;
 	}
 
-	vector<double> GetEnergy(){
-		return energy;
-	}
+	vector<double> GetEnergy(){ return energy; }
 
 	friend ostream& operator<<( ostream& os, const MCPulse& a);
 
@@ -107,17 +52,6 @@ private:
 
 	vector<double> energy;
 };
-
-ostream& operator<<( ostream& os, const MCPulse& a){
-
-	os << a.time;
-
-	vector<double> en = a.energy;
-	for( unsigned int i=0; i<en.size(); i++ )
-		os << ' ' << en[i];
-
-	return os;
-}
 
 
 #endif
--- analysis/include/MCPulseArray.h
+++ analysis/include/MCPulseArray.h
@@ -11,20 +11,11 @@ class MCPulseArray{
 
 public:
 
-	MCPulseArray(){
-		head = pulseArray.begin();
-	}
+	MCPulseArray();
 
-	MCPulseArray( const MCPulseArray& a ){
-		pulseArray = a.pulseArray;
-		head = pulseArray.begin();
-	}
+	MCPulseArray( const MCPulseArray& a );
 
-	MCPulseArray& operator=( const MCPulseArray& a){
-		pulseArray = a.pulseArray;
-		head = pulseArray.begin();
-		return *this;
-	}
+	MCPulseArray& operator=( const MCPulseArray& a);
 
 	~MCPulseArray(){}
 
@@ -36,91 +27,19 @@ public:
 
 	friend ostream& operator<<( ostream& os, MCPulseArray& a);
 
-	double GetNextInteractionTime(){
-		if( head!=pulseArray.end() )
-			return (*head).GetTime();
-		return -1;
-	}
+	double GetNextInteractionTime();
 
-	MCPulse FindPrimaryInteraction( double start, double window){
+	MCPulse FindPrimaryInteraction( double start, double window);
 
-		MCPulse val;
-
-		if( pulseArray.size()==0 )
-			return val;
-
-		while( head != pulseArray.end() && (*head).GetTime()-start < window ){
-			val = val + *head;
-			head++;
-		}
-
-		return val;
-	}
-
-
-	MCPulse FindCoincidentInteraction( double start, double win_coin, double win_daq){
-
-		MCPulse val;
-
-		// locate the beginning of the event.
-		//
-		vector<MCPulse>::iterator beg = head;
-
-		for( beg=pulseArray.begin(); beg!=pulseArray.end(); beg++ ){
-			double t = beg->GetTime();
-			if( abs(start-t) < win_coin ){
-				break;
-			}
-			else if( t-start>win_coin ){
-				return val;
-			}
-		}
-
-		// locate the end of the event.
-		//
-		vector<MCPulse>::iterator end = head;
-
-		for( end=pulseArray.begin(); end!=pulseArray.end(); end++ ){
-			double t = end->GetTime();
-			if( t-start > win_daq ){
-				break;
-			}
-		}
-
-		// Sum the events.
-		// 
-		for( auto itr = beg; itr!=end; itr++ ){
-			val += *itr;
-		}
-
-		head = end;
-
-		return val;
-	}
-	
+	MCPulse FindCoincidentInteraction( double start, double win_coin, double win_daq);
 
 private:
 
 	vector<MCPulse> pulseArray;
 
 	vector<MCPulse>::iterator head;
+
 };
 
-ostream& operator<<( ostream& os, MCPulseArray& a){
-	vector<MCPulse>::iterator itr;
-	unsigned int nType;
-	for( itr = a.pulseArray.begin(); itr!=a.pulseArray.end(); itr++){
-		os << itr->GetTime() << '\t';
-		nType = itr->GetNType();
-	}
-	os << endl;
-	for( unsigned int i=0; i<nType; i++){
-		for( itr = a.pulseArray.begin(); itr!=a.pulseArray.end(); itr++){
-			os << itr->GetEnergy()[i] << '\t';
-		}
-		os << endl;
-	}
-	return os;
-}
 
 #endif
